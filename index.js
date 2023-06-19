5537e84e75e37167f80e8a22ae4b681f23b2bb46
Ezequiel <ezequiel.signorini@gmail.com>
2023-06-19 18:27:51 -0300

    adding security to images

--- index.js
+++
@@ -1,5 +0,0 @@
-// Cuando se baja al nav se le agrega la clase "abajo"
-window.addEventListener("scroll", function(){
-    var header = document.querySelector("nav");
-    header.classList.toggle("abajo", window.scrollY > 0);
-})
